bc3d20a7576075ea331689840b7cf575e693aff9
rahulmohonto <rahulmohonto1999@gmail.com>
2021-04-03 05:49:09 +0600

    data uploaded to database

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ObjectID = require('mongodb').ObjectID;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4200
 
 
 app.use(cors());
@@ -15,4 +15,37 @@ console.log(process.env.DB_USER)
 
 app.get('/', (req, res) => {
     res.send(` Hello Rahul Mohonto Welcome to port ${port}`)
+})
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.adxso.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const productCollection = client.db("full-stack-assignment-database").collection("products");
+    console.log("database connected successfully")
+
+    app.post('/addProduct', (req, res) => {
+        const newProduct = req.body;
+        console.log('adding products', newProduct);
+        productCollection.insertOne(newProduct)
+            .then(result => {
+                console.log(result.insertedCount);
+                res.send(result.insertedCount > 0)
+            })
+
+    })
+
+    app.get('/products', (req, res) => {
+        productCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+});
+
+
+
+app.listen(port, () => {
+    console.log(`welcome rahul Mohonto at port http://localhost:${port}`)
 })
