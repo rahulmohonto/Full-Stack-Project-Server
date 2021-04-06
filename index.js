e1eb182c7d52e0202704431a508c2dc103f40b0a
rahulmohonto <rahulmohonto1999@gmail.com>
2021-04-06 20:19:29 +0600

    adada

--- index.js
+++ index.js
@@ -55,14 +55,19 @@ client.connect(err => {
 
 
     app.delete('/deleteProduct/:id', (req, res) => {
-        const id = ObjectID(req.params.id);
+        // const id = ObjectID(req.params.id);
         console.log('delete this product', id)
-        productCollection.findOneAndDelete({ _id: req.params.id })
+        // productCollection.findOneAndDelete({ _id: req.params.id })
+        //     .then(result => {
+        //         res.send(result.deletedCount > 0)
+        //         console.log(result)
+        //     })
+        //     .catch(err => console.error(`Failed to find and delete document: ${err}`))
+        productCollection.deleteOne({ _id: ObjectID(req.params.id) })
+
             .then(result => {
                 res.send(result.deletedCount > 0)
-                console.log(result)
             })
-            .catch(err => console.error(`Failed to find and delete document: ${err}`))
     })
 
 });
